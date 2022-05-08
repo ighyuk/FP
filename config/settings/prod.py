968722fac68e22005ef28b85450b033c92c61607
stimfirebat <hmlim97@naver.com>
2022-05-09 05:39:06 +0900

    STATIC_ROOT 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,5 @@
 from .base import *
 
-ALLOWED_HOSTS = ['13.124.180.12']
+ALLOWED_HOSTS = ['13.124.180.12', 'kumohfp.kro.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
--- manage.py
+++ manage.py
@@ -21,22 +21,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    """server = WebSocketServer('192.168.35.204', 8001, SimpleEcho)
-    server.serve_forever()"""
-
-class SimpleEcho(WebSocket):
-    def handle(self):
-        msg = self.data
-        img = cv2.imdecode(np.fromstring(base64.b64decode(msg.split(',')[1]), np.uint8), cv2.IMREAD_COLOR)
-        cv2.imshow('image', img)
-        cv2.waitKey(1)
-
-    def connected(self):
-        print(self.address, 'connected')
-
-    def handle_close(self):
-        print(self.address, 'closed')
-
 
 if __name__ == '__main__':
     main()
